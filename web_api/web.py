6941549af995839df4786ec9a80f8fdcfd9c60ff
jan-tomsa <jan.tomsa.1976@gmail.com>
2019-03-04 00:57:14 +0100

    Added test of pgpio functions

--- web_api/web.py
+++ web_api/web.py
@@ -1,5 +1,10 @@
 from flask import Flask
+import pigpio
+
 app = Flask(__name__)
+
+pi = pigpio.pi() # Connect to local Pi.
+pi.set_mode(21, pigpio.INPUT)
 
 @app.route('/')
 def index():
@@ -8,3 +13,7 @@ def index():
 @app.route('/motor/<motor_id>/')
 def motor(motor_id):
     return 'Motor {}'.format(motor_id)
+
+
+@app.route('/test/')
+    pi.write(21, 1) # on
